286ce000a6f72b662b17d2e61432487added06aa
tomoropy <ucchi1910@gmail.com>
2023-04-19 21:06:54 +0900

    [add] sample code を追加

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	apiKey := os.Getenv("API_KEY")
+	apiSecretKey := os.Getenv("API_SECRET_KEY")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
+
+	config := oauth1.NewConfig(apiKey, apiSecretKey)
+	token := oauth1.NewToken(accessToken, accessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	data, err := ioutil.ReadFile("merged.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tweet, _, err := client.Statuses.Update(string(data), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Posted tweet: %s\n", tweet.Text)
+}
